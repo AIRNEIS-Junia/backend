8cf53f66538ccdabaac30cd651725891e5eb6b6e
Arwantys <mathis@lorthios.email>
2024-01-08 14:14:02 +0100

    Initialize project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthController } from './application/controllers/auth.controller';
+import { AuthService } from './domain/services/auth.service';
+import { PrismaService } from './domain/services/prisma.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AuthController],
+  providers: [PrismaService, AuthService],
 })
 export class AppModule {}
