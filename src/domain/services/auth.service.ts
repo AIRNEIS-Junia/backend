02bf86187f64c0f86f6dd0efae0dce004b2863bc
unk <unk@unk.com>
2024-01-08 14:15:50 +0100

    Initialize project

--- src/domain/services/auth.service.ts
+++ src/domain/services/auth.service.ts
@@ -1,8 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { UserRepository } from '../../infrastructure/repositories/user.repository';
+import { AuthLoginDto } from '../../application/dto/auth.dto';
 
 @Injectable()
 export class AuthService {
-  async login() {
+  constructor(private readonly userRepository: UserRepository) {}
+  async login(data: AuthLoginDto) {
+    const findEmail = await this.userRepository.findByEmail(data.email);
+
+    if (!findEmail) throw new NotFoundException('EMAIL_OR_PASSWORD_WRONG');
+
     return {
       status: 'success',
       message: 'login working',
